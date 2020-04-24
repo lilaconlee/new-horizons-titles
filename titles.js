24f400e0d42bfb8e8e50367dcdce29d57910551e
lilaconlee <lila.conlee@gmail.com>
2020-04-23 20:52:41 -0500

    Add lists

--- titles.js
+++ titles.js
@@ -2,12 +2,41 @@ const _ = require("lodash");
 
 import "./main.scss";
 
-const { starts, ends } = require("./data.json");
+// TODO padding at bottom of lists
+// TODO alphabetical "It"
 
-const colorClassNames = ["blue", "pink", "green", "purple"];
+let { starts, ends } = require("./data.json");
 
 const getTitle = () => _.sample(starts) + " " + _.sample(ends);
 
-const textEl = document.querySelector(".text");
-textEl.textContent = getTitle();
-document.querySelector(".container").classList.add(_.sample(colorClassNames));
+const createListItem = (word) => {};
+
+const populateListEl = (listEl, wordList) => {
+  wordList.forEach((word) => {
+    let itemEl = document.createElement("li");
+    itemEl.innerHTML = word;
+    itemEl.classList.add("item");
+    listEl.appendChild(itemEl);
+  });
+};
+
+let numNbsp = 5;
+while (numNbsp > 0) {
+  starts.push("&nbsp");
+  ends.push("&nbsp");
+  numNbsp--;
+}
+
+// if (ends.length > starts.length) {
+// let extras = ends.length - starts.length;
+// while (extras > 0) {
+// starts.push("&nbsp");
+// extras--;
+// }
+// }
+
+const startsEl = document.getElementById("starts");
+const endsEl = document.getElementById("ends");
+
+populateListEl(startsEl, starts);
+populateListEl(endsEl, ends);
